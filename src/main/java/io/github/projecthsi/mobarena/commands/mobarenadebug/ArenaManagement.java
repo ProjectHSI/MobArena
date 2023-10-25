42809b649e3c6097a8aeee0b3e08bd569e7a3af3
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-25 12:03:59 +1000

    createDebugArena command

--- src/main/java/io/github/projecthsi/mobarena/commands/mobarenadebug/ArenaManagement.java
+++ src/main/java/io/github/projecthsi/mobarena/commands/mobarenadebug/ArenaManagement.java
@@ -1,7 +1,10 @@
 package io.github.projecthsi.mobarena.commands.mobarenadebug;
 
+import io.github.projecthsi.mobarena.arena.Arena;
+import io.github.projecthsi.mobarena.arena.MobSpawnEntry;
 import io.github.projecthsi.mobarena.containers.ArenaContainer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.NotNull;
 
 public class ArenaManagement {
@@ -18,5 +21,19 @@ public class ArenaManagement {
             sender.sendMessage("mad error; invalid arena");
             return false;
         }
+    }
+
+    static boolean createDebugArena(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+        if (args.length != 0 + 1) {
+            sender.sendMessage("mad error; must have no arguments");
+            return false;
+        }
+
+        Arena arena = new Arena();
+
+        // mob spawn entries
+        MobSpawnEntry zombie = new MobSpawnEntry(EntityType.ZOMBIE, 1, 1, 1, "arena");
+
+        return true;
     }
 }
