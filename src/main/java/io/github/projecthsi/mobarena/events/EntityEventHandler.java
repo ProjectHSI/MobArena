f7948d2bfc18de5b444ac16148cdb8ae22ff40f4
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-30 08:11:23 +1000

    change EntityEventHandler to be about, entities

--- src/main/java/io/github/projecthsi/mobarena/events/EntityEventHandler.java
+++ src/main/java/io/github/projecthsi/mobarena/events/EntityEventHandler.java
@@ -1,21 +1,29 @@
 package io.github.projecthsi.mobarena.events;
 
+import io.github.projecthsi.mobarena.containers.EntityContainer;
 import io.github.projecthsi.mobarena.containers.PlayerContainer;
+import org.bukkit.entity.Mob;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 import static io.github.projecthsi.mobarena.plugin.MobArena.getInstance;
 
 public class EntityEventHandler implements Listener {
     @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        getInstance().getLogger().info(String.format("Player death; %s", event.getPlayer().getName()));
+    public void onEntityDeath(EntityDeathEvent event) {
+        Mob mob = ( Mob ) event.getEntity();
 
-        if (!PlayerContainer.getTrackedPlayers().containsKey(event.getPlayer())) {
+        if (mob == null) {
             return;
         }
 
-        PlayerContainer.getTrackedPlayers().get(event.getPlayer()).playerDeath(event.getPlayer());
+        getInstance().getLogger().info(String.format("Entity death; %s", event.getEntity().getName()));
+
+        if (!PlayerContainer.getTrackedPlayers().containsKey(mob)) {
+            return;
+        }
+
+        EntityContainer.getTrackedMobs().get(mob).entityDeath(mob);
     }
 }
