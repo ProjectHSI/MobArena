e307e3798a521bfb2f5ad953aea23983baad49c3
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-30 08:10:51 +1000

    change type of EntityContainer.trackedMobs

--- src/main/java/io/github/projecthsi/mobarena/containers/EntityContainer.java
+++ src/main/java/io/github/projecthsi/mobarena/containers/EntityContainer.java
@@ -1,17 +1,18 @@
 package io.github.projecthsi.mobarena.containers;
 
+import io.github.projecthsi.mobarena.arena.Arena;
 import org.bukkit.entity.Mob;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EntityContainer {
-    private static ArrayList<Mob> trackedMobs = new ArrayList<>();
+    private static HashMap<Mob, Arena> trackedMobs = new HashMap<>();
 
-    public static void setTrackedMobs(ArrayList<Mob> newTrackedMobs) {
+    public static void setTrackedMobs(HashMap<Mob, Arena> newTrackedMobs) {
         trackedMobs = newTrackedMobs;
     }
 
-    public static ArrayList<Mob> getTrackedMobs() {
+    public static HashMap<Mob, Arena> getTrackedMobs() {
         return trackedMobs;
     }
 }
