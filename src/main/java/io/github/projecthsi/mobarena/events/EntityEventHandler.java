f5ab3d7b1b6d3a5db769e346bfcc1bbb6c79c122
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-30 07:53:34 +1000

    clone PlayerEventHandler.java

---
+++ src/main/java/io/github/projecthsi/mobarena/events/EntityEventHandler.java
@@ -0,0 +1,21 @@
+package io.github.projecthsi.mobarena.events;
+
+import io.github.projecthsi.mobarena.containers.PlayerContainer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import static io.github.projecthsi.mobarena.plugin.MobArena.getInstance;
+
+public class EntityEventHandler implements Listener {
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        getInstance().getLogger().info(String.format("Player death; %s", event.getPlayer().getName()));
+
+        if (!PlayerContainer.getTrackedPlayers().containsKey(event.getPlayer())) {
+            return;
+        }
+
+        PlayerContainer.getTrackedPlayers().get(event.getPlayer()).playerDeath(event.getPlayer());
+    }
+}
