05081862a34a4ac8cce235fe73681c96badc2eab
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-30 07:41:15 +1000

    rename EntityEventHandler.java to PlayerEventHandler.java (adjusting corresponding class names)

--- src/main/java/io/github/projecthsi/mobarena/EventSystem.java
+++ src/main/java/io/github/projecthsi/mobarena/EventSystem.java
@@ -1,11 +1,12 @@
 package io.github.projecthsi.mobarena;
 
+import io.github.projecthsi.mobarena.events.PlayerEventHandler;
 import io.github.projecthsi.mobarena.plugin.MobArena;
 
 import static org.bukkit.Bukkit.getServer;
 
 public class EventSystem {
     public static void registerEvents() {
-        getServer().getPluginManager().registerEvents(new io.github.projecthsi.mobarena.events.EntityEventHandler(), MobArena.getInstance());
+        getServer().getPluginManager().registerEvents(new PlayerEventHandler(), MobArena.getInstance());
     }
 }
--- src/main/java/io/github/projecthsi/mobarena/events/PlayerEventHandler.java
+++ src/main/java/io/github/projecthsi/mobarena/events/PlayerEventHandler.java
@@ -7,7 +7,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 import static io.github.projecthsi.mobarena.plugin.MobArena.getInstance;
 
-public class EntityEventHandler implements Listener {
+public class PlayerEventHandler implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         getInstance().getLogger().info(String.format("Player death; %s", event.getPlayer().getName()));
