7d21a5572196bebbf1140a4347555a59025410bd
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-30 17:39:17 +1000

    Fix up class declaration

--- src/main/java/io/github/projecthsi/mobarena/events/PlayerEventHandler.java
+++ src/main/java/io/github/projecthsi/mobarena/events/PlayerEventHandler.java
@@ -2,13 +2,14 @@ package io.github.projecthsi.mobarena.events;
 
 import io.github.projecthsi.mobarena.containers.PlayerContainer;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import static io.github.projecthsi.mobarena.plugin.MobArena.getInstance;
 
-public class PlayerEventHandler implements Listener {
-    @EventHandler
+public class PlayerEventHandler implements Listener
+{
     public void onPlayerDeath(PlayerDeathEvent event) {
         getInstance().getLogger().info(String.format("Player death; %s", event.getPlayer().getName()));
 
