e4d9183548b604c725277e22d8d11c086894e923
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-31 20:28:25 +1000

    extract mob death handler into its own function

--- src/main/java/io/github/projecthsi/mobarena/events/EntityEventHandler.java
+++ src/main/java/io/github/projecthsi/mobarena/events/EntityEventHandler.java
@@ -1,6 +1,7 @@
 package io.github.projecthsi.mobarena.events;
 
 import io.github.projecthsi.mobarena.containers.EntityContainer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,6 +10,25 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import static io.github.projecthsi.mobarena.plugin.MobArena.getInstance;
 
 public class EntityEventHandler implements Listener {
+    private void mobDeathEvent(Entity entity) {
+        Mob mob = ( Mob ) entity;
+
+        if (mob == null) {
+            getInstance().getLogger().info(String.format("Non-mob Entity death; %s", entity.getName()));
+            return;
+        }
+
+        getInstance().getLogger().info(String.format("Entity death; %s", entity.getName()));
+
+        if (!EntityContainer.containsTrackedMob(mob)) {
+            return;
+        }
+
+        getInstance().getLogger().info(String.format("Tracked mob; %s", entity.getName()));
+
+        EntityContainer.getTrackedMobArena(mob).entityDeath(mob);
+    }
+
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
         getInstance().getLogger().info(event.getClass().getName());
