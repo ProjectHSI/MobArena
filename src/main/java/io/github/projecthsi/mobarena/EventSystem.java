db4d3c1e2e9e3a738e95b6de2b564d1cc344fc67
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-10-31 17:30:18 +1000

    Register EntityEventHandler.

--- src/main/java/io/github/projecthsi/mobarena/EventSystem.java
+++ src/main/java/io/github/projecthsi/mobarena/EventSystem.java
@@ -1,5 +1,6 @@
 package io.github.projecthsi.mobarena;
 
+import io.github.projecthsi.mobarena.events.EntityEventHandler;
 import io.github.projecthsi.mobarena.events.PlayerEventHandler;
 import io.github.projecthsi.mobarena.plugin.MobArena;
 
@@ -8,5 +9,6 @@ import static org.bukkit.Bukkit.getServer;
 public class EventSystem {
     public static void registerEvents() {
         getServer().getPluginManager().registerEvents(new PlayerEventHandler(), MobArena.getInstance());
+        getServer().getPluginManager().registerEvents(new EntityEventHandler(), MobArena.getInstance());
     }
 }
