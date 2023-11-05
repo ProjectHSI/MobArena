f06d42f3a8fd31d32dfc50fbab8bd4a057a802f6
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-11-05 13:34:53 +1000

    show calculated versions

--- determineReleaseVersion.sh
+++ determineReleaseVersion.sh
@@ -2,23 +2,27 @@
 
 usingNonMainBranch=false
 
-ls /usr/bin/
-
 commitHash=$(/usr/bin/git rev-parse --short HEAD)
 branchName=$(/usr/bin/git rev-parse --abbrev-ref HEAD)
+
+echo "Calculated Version (Commit Hash): '$($commitHash)'."
+echo "Calculated Version (Branch Name): '$($branchName)'."
 
 version=""
 
 if [[ $branchName == "main" ]]; then
-    usingNonMainBranch=false
+  echo "Not on Main branch."
+  usingNonMainBranch=true
 fi
 
 version="$($commitHash)"
 
+echo "Calculated Version (Stage 1): '$($commitHash)'."
+
 if [[ $usingNonMainBranch = true ]]; then
-    version="$($version)-$($branchName)"
+  version="$($version)-$($branchName)"
 fi
 
-echo "Calculated Version: '$($version)'."
+echo "Calculated Version (Final): '$($version)'."
 
 echo "VersionID=$($version)" >> "$GITHUB_OUTPUT"
