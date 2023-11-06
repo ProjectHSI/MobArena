69e42761e523c781eaab8e795a62510165185e32
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-11-06 18:37:31 +1000

    Convert class into record.

--- src/main/java/io/github/projecthsi/mobarena/FillArea.java
+++ src/main/java/io/github/projecthsi/mobarena/FillArea.java
@@ -3,18 +3,4 @@ package io.github.projecthsi.mobarena;
 import org.bukkit.Location;
 import org.bukkit.World;
 
-public class FillArea {
-    private final Location pos1;
-    private final Location pos2;
-    private final World world;
-
-    public FillArea(Location pos1, Location pos2, World world) {
-        this.pos1 = pos1;
-        this.pos2 = pos2;
-        this.world = world;
-    }
-
-    public Location getPos1() { return pos1; }
-    public Location getPos2() { return pos2; }
-    public World getWorld() { return world; }
-}
+public record FillArea(Location pos1, Location pos2, World world) {}
